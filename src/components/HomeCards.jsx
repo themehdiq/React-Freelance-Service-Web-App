97985c08bdfc02d3c2e3095fdec7ed64ad4e3e2c
themehdiq <qazzih.mehdi97@gmail.com>
2024-04-06 12:57:43 +0000

    adding services json file and browsing services section

--- src/components/HomeCards.jsx
+++ src/components/HomeCards.jsx
@@ -7,27 +7,29 @@ const HomeCards = () => {
       <div className="container-xl lg:container m-auto">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg">
           <Card>
-            <h2 className="text-2xl font-bold">For Developers</h2>
+            <h2 className="text-2xl font-bold">For Clients</h2>
             <p className="mt-2 mb-4">
-              Browse our React jobs and start your career today
+              Browse services to find the perfect freelancer for your project
+              needs.
             </p>
             <a
               href="/jobs.html"
               className="inline-block bg-black text-white rounded-lg px-4 py-2 hover:bg-gray-700"
             >
-              Browse Jobs
+              Browse Services
             </a>
           </Card>
           <Card bg="bg-green-200">
-            <h2 className="text-2xl font-bold">For Employers</h2>
+            <h2 className="text-2xl font-bold">For Freelancers</h2>
             <p className="mt-2 mb-4">
-              List your job to find the perfect developer for the role
+              List your services to offer your expertise and connect with
+              potential clients.
             </p>
             <a
               href="/add-job.html"
-              className="inline-block bg-indigo-500 text-white rounded-lg px-4 py-2 hover:bg-indigo-600"
+              className="inline-block bg-green-700 text-white rounded-lg px-4 py-2 hover:bg-indigo-600"
             >
-              Add Job
+              Add Service
             </a>
           </Card>
         </div>
